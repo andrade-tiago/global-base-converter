ecffdfab468ec78b4ba40079fa6b6339d6d7f32d
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-11-02 16:02:36 -0300

    :wrench: chore: config imports

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as CustomBase } from './custom-base';
+export { default as CustomBaseNumber } from './custom-base-number';
+export { default as BaseConverter } from './base-converter';
+export * from './defaults';
