86d57b19cdb9145b4d22a7c0ed6a1ba567f92bb6
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-11-05 18:00:59 -0300

    :wastebasket: remove: base converter class file & references

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { default as CustomBase } from './custom-base';
 export { default as CustomBaseNumber } from './custom-base-number';
-export { default as BaseConverter } from './base-converter';
 export * from './defaults';
